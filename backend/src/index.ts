c7bf90f2ab71482ba9fc28bff333a1617ba55f9b
JayantRautela <jayantrautela0205@gmail.com>
2025-11-13 22:36:49 +0530

    manage directory and frontend init

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -4,12 +4,13 @@ import "dotenv/config";
 const client = new GoogleGenAI({});
 
 const main = async () => {
+    // const content: Content[]
     const response = await client.models.generateContent({
         model: "gemini-2.5-flash",
         contents: "What is ai"
     });
 
-    console.log("RESPONSE : ", response.data);
+    console.log("RESPONSE : ", response.text);
 }
 
 main();
