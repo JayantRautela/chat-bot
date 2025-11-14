4c748f99faa7a1ed4f23d6c0abf0c747144137dc
JayantRautela <jayantrautela0205@gmail.com>
2025-11-14 23:24:56 +0530

    fix types

--- backend/src/lib/chat.ts
+++ backend/src/lib/chat.ts
@@ -3,14 +3,14 @@ import Groq from "groq-sdk";
 const client = new Groq({ apiKey: process.env.GRQO_API_KEY });
 
 export const getResponse = async (message = "Explain me what is javascript") => {
-    const messages = [{}];
+    const messages: Groq.Chat.Completions.ChatCompletionMessageParam[] = [];
     messages.push({
         role: 'system',
         content: `You are a smart personal assistant. 
             Your function is to generate responses based on the data you have. 
             Only provide responses in text format in plain english. 
             Only return the answer and not the question.
-            Current Date and Time = ${Date.now().toLocaleString()}
+            Current Date and Time = ${new Date().toLocaleString()}
             
             For example: 
             Q. What is the capital of India?
@@ -20,11 +20,14 @@ export const getResponse = async (message = "Explain me what is javascript") => 
             Q. What is the name of the spcae agency of America?
             A. The name of the space agency of America is NASA(National Aeronautics and Space Administration).
         `
-    }, {
+    });
+
+    messages.push({
         role: 'user',
         content: message
     });
-    try {
+
+    try {;
         const response = await client.chat.completions.create({
             model: 'llama-3.1-8b-instant',
             messages: messages
